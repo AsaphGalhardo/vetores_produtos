8616fe914fccb5d2f40fabadf029f5e8a5c70f44
AsaphGalhardo <asaphgalhardo@gmail.com>
2023-03-01 21:55:30 -0300

    Criando repositorio para produtos

---
+++ vetores_produtos/Program.cs
@@ -0,0 +1,14 @@
+﻿//criar variaveis
+string[] vet_prod = new string[3];
+string[] vet_prodtipo = new string[3];
+string[] vet_prodvalor = new double[3];
+
+int i = 0;
+
+for (i = 0; i <=2; i++)
+{
+            Cins
+}
+
+    
+
